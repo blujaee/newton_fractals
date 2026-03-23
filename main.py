cbf25054466f19e821fc6430c158fd4952d157a0
audrey ˚ʚ♡ɞ˚ <119465635+blujaee@users.noreply.github.com>
2026-03-23 14:11:44 -0400

    linspace

--- main.py
+++ main.py
@@ -2,14 +2,33 @@ from newtons_method import newtons_method
 import numpy as np
 import numpy.polynomial.polynomial as poly
 
+
+x = np.linspace(-1,1,3)
+y = x
+
+X, Y = np.meshgrid(x, y)
+
+init_guesses = X + Y * 1j
+
+rows = init_guesses.shape[0]
+cols = init_guesses.shape[1]
+roots = np.zeros((rows,cols), dtype=complex)
+    
 degree = int(input("what degree is the polynomial? "))
 coeffs = []
 for i in range(degree+1):
     coeffs.append(int(input("Coefficient on power " + str(i) + " term: ")))
 print(coeffs)
-init_guess = int(input("Enter an initial guess: "))
 maxit = 2000
 TOL = 10e-13
 p = poly.Polynomial(coeffs)
 print(p)
-newtons_method(init_guess, maxit, TOL, p)
+
+for i in range(rows):
+    for j in range(cols):
+        init_guess = init_guesses[i,j]
+        converged, root = newtons_method(init_guess, maxit, TOL, p)
+        if converged == True:
+            roots[i,j] = root
+        else:
+            roots[i,j] = np.nan
--- newtons_method.py
+++ newtons_method.py
@@ -2,22 +2,24 @@ import numpy as np
 import numpy.polynomial.polynomial as poly
 
 def newtons_method(init_guess, maxit, TOL, p):
+    converged = True
+    deriv = p.deriv()
     for k in range(maxit):
         f_x = p(init_guess)
-        deriv = p.deriv()
         f_prime = deriv(init_guess)
         if abs(f_prime) < TOL:
             print("failed to converge due to division by 0")
+            converged = False
             break
         new_guess = init_guess - (f_x / f_prime)
         if abs(new_guess-init_guess) < TOL:
             print("Converged at ", k, " iterations.")
             print("Root is: ", new_guess)
             break
-        
         init_guess = new_guess
         if k == maxit-1:
             print("failed to converge, hit maxit")
+            converged = False
             break
     
-    return init_guess
+    return converged, init_guess
