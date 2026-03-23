d4e0c41c8c96643e60d593629410729b89c29880
audrey ˚ʚ♡ɞ˚ <119465635+blujaee@users.noreply.github.com>
2026-03-23 13:09:32 -0400

    better name and more error messages

--- newtons_method.py
+++ newtons_method.py
@@ -11,16 +11,18 @@ def newtons_method(init_guess, maxit, TOL):
         deriv = p.deriv()
         f_prime = deriv(init_guess)
         if abs(f_prime) < TOL:
+            print("failed to converge due to division by 0")
             break
-        
         new_guess = init_guess - (f_x / f_prime)
-        
         if abs(new_guess-init_guess) < TOL:
             print("Converged at ", k, " iterations.")
             print("Root is: ", new_guess)
             break
         
         init_guess = new_guess
+        if k == maxit-1:
+            print("failed to converge, hit maxit")
+            break
     
     return init_guess
         
